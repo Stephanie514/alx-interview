eb00ceb85c248f88fbd6d89ed732238f708d6ccb
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-14 15:17:08 +0000

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,10 +2,11 @@
 """ function def pascal_triangle(n): that returns a
 list of lists of integers"""
 
+
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
+
     triangle = []
     for line in range(n):
         row = [1]  # First element in each row is always 1
@@ -13,5 +14,5 @@ def pascal_triangle(n):
             # Calculating the next element in the row using the previous row
             row.append(row[i - 1] * (line - i + 1) // i)
         triangle.append(row)
-    
+
     return triangle
